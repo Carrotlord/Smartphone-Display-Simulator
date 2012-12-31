a7d776eebbbadd320f1f925aa6ce460d6cf9ee7c
Oliver Chu <j.oliverchu@berkeley.edu>
2012-12-30 22:47:08 -0800

    Create CONSTANTS.java
    A list of numerical constants and other types of Java Object constants.

---
+++ CONSTANTS.java
@@ -0,0 +1,10 @@
+package droidemulator;
+
+/**
+ * @author Oliver Chu
+ */
+public class CONSTANTS {
+    /** This is exactly 2/3s of the size of a regular Android device. */
+    public static final short DROID_WINDOW_HEIGHT = 533;
+    public static final short DROID_WINDOW_WIDTH = 320;
+}
